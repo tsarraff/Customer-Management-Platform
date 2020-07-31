ec60a8ab7cd88f8c65677c4f266770ed356c539f
Teja Sarraff <emailforteja@gmail.com>
2020-07-30 22:17:42 -0700

    Updated Database code

--- accounts/models.py
+++ accounts/models.py
@@ -42,10 +42,11 @@ class Order(models.Model):
     )
     customer = models.ForeignKey(Customer, null=True,on_delete=models.SET_NULL)
     product = models.ForeignKey(Product, null=True, on_delete=models.SET_NULL)
-    
-
     date_created = models.DateTimeField(auto_now_add=True, null=True)
     status = models.CharField(max_length=200, null=True, choices=STATUS)
+    def __str__(self):
+        return self.product.name
+
 
     
     
